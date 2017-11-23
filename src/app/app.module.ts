e8b0351f17bbb59bfd27d2e3ff6d257f501fde53
jeissonmgz <jeissonmgz@gmail.com>
2017-11-23 12:43:03 -0500

    Creacion del componente heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HomeComponent,
     NavbarComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     APP_ROUTING,
